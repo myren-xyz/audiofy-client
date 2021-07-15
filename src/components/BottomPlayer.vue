626ac3b3a69011a5f0c12024158834c2466be4ea
= <pesaregoal@gmail.com>
2021-07-15 18:46:58 +0430

    fix: separate player controller component

--- src/components/BottomPlayer.vue
+++ src/components/BottomPlayer.vue
@@ -1,9 +1,9 @@
 <template>
 <div v-show="song.title" class="bplayer" id="wrapper-player">
     <!-- <audio id="aud" ref="audio" controls></audio> -->
-    <div id="audiofy-player" @click="expand">
+    <div id="audiofy-player">
         <div id="gesture">
-            <div id="darbar" class="flex">
+            <div id="darbar" class="flex" @click="expand">
                 <div class="avatar" id="song--avatar">
                     <img :src="song.picURL" >
                 </div>
@@ -11,38 +11,29 @@
                     <h5 id="song-title">{{song.title}}</h5>
                     <p id="song-by">{{song.by}}</p>
                 </div>
+                <Control />
             </div>
-            <div id="pp" @click="pp">
-                <ion-icon v-if="!this.isPlaying" name="play"></ion-icon>
-                <ion-icon v-if="this.isPlaying" name="pause"></ion-icon>
+            <div id="pp" @click="changePlayingState">
+                <ion-icon v-if="!player.isPlaying" name="play"></ion-icon>
+                <ion-icon v-if="player.isPlaying" name="pause"></ion-icon>
             </div>
         </div>
     </div>
 </div>
 </template>
+
 <script>
 import {mapState} from 'vuex';
 import Hls from 'hls.js';
+import Control from '@/components/BottomPlayer/Control.vue'
+
 const au = new Audio();
 au.preload = "metadata";
 export default {
-    data: function(){
-        return {
-            result: [],
-            delay: 700,
-            clicks: 0,
-            timer: null,
-            isPlaying: false,
-
-            dist: null,
-            startX: null,
-            startY: null,
-            threshold: 150,
-            allowedTime: 200,
-            elapsedTime: null,
-            startTime: null
-        }
+    components: {
+        Control
     },
+
     mounted() {
         this.$store.subscribe((mutation)=>{
             if(mutation.type === 'setSong') {
@@ -57,8 +48,16 @@ export default {
                     audio.src = stream;
                 }
                 
-                au.play()
-                this.isPlaying = true;
+                // au.play()
+                this.$store.commit('changePlayingState')
+            }
+
+            if(mutation.type == 'changePlayingState') {
+                if (this.player.isPlaying){
+                    au.play()
+                } else {
+                    au.pause()
+                }
             }
         })
         // au.addEventListener('timeupdate', ()=>{
@@ -69,16 +68,14 @@ export default {
     methods:{
         seekto: function(e){
             au.currentTime = au.duration*e.layerX/this.$el.clientWidth
-            // console.log(this.$el.clientWidth);
         },
-        pp: function(){
-            if (this.isPlaying){
-                au.pause()
-                this.isPlaying = false
-            } else {
-                au.play()
-                this.isPlaying = true
-            }
+
+        changeRepeatState: function(){
+            this.$store.commit('changeRepeatState')
+        },
+
+        changePlayingState: function() {
+            this.$store.commit('changePlayingState')
         },
 
         expand: function() {
@@ -90,15 +87,17 @@ export default {
             document.getElementById('pp').style = "width: 0; height:0;"
             document.getElementById('song-title').style = "font-size: 24px;text-align:center"
             document.getElementById('song-by').style = "text-align:center"
+            document.getElementById('player--control--wrapper').style = "display:flex"
         }
     },
-    computed: mapState(["song"])
+    computed: mapState(["song","player"])
 }
 import store from '../store/index'
+
 au.addEventListener('ended', ()=>{
     let currentSongId = store.state.song.id +1;
     let nextSong = store.state.songs.filter(song => song.id == currentSongId);
-    let repeat = store.state.opts.repeat;
+    let repeat = store.state.player.repeat;
     if(nextSong[0] != null && repeat == false){
         store.commit('setSong', nextSong[0])
     }
@@ -128,14 +127,14 @@ au.addEventListener('ended', ()=>{
     display: flex;
     place-content: center;
     place-items: center;
-    transition: 1s all ease-in-out;
+    transition: 0.6s all ease-in-out;
 }
 .avatar img {
     height: 100%;
     width: 100%;
 }
 .bplayer{
-    transition: 1s all ease-in-out;
+    transition: 0.6s all ease-in-out;
     position: fixed;
     width: 100%;
     height: 54px;
@@ -164,11 +163,11 @@ audio{
 #song-by{
     color: #969696;
     font-size: 12px;
-    transition: 1s all ease-in-out;
+    transition: 0.6s all ease-in-out;
 }
 #song-title{
     font-size: 14px;
-    transition: 1s all ease-in-out;
+    transition: 0.6s all ease-in-out;
 }
 #gesture{
     height: 54px;
@@ -190,6 +189,33 @@ audio{
     place-items: center;
     place-content: center;
     font-size: 32px;
+    color: #ffc857
+}
+#player--control--wrapper {
+    display: none;
+    width: 86%;
+    height: 100%;
+    justify-content: space-between;
+    align-items: center;
+    color: #c1c1c1
+}
+#player--control--wrapper div {
+    min-width: 46px;
+    min-height: 46px;
+    display: flex;
+    place-content: center;
+    place-items: center;
+}
+.base-1 {
+    font-size: 24px;
+}
+.base-2 {
+    font-size: 30px;
+}
+.base-3 {
+    font-size: 36px;
+}
+.active {
     color: #ffc857
 }
 @media screen and (min-width: 728px){
