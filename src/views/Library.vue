5602a0a642297d65285732914617e92780be0499
= <pesaregoal@gmail.com>
2021-07-16 03:28:18 +0430

    fix: up to date with UI

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,25 +1,77 @@
 <template>
-  <div>
-    <div class="playlist-items">
-      <p class="titles">listening history</p>
+  <div class="library">
+    <h2>Listening History</h2>
+    <div class="slider">
+        <Song v-for="song in songs" :song="song" :key="song.id"/>
     </div>
-    <div class="playlist-items">
-      <p class="titles">your playlists</p>
+    
+    <h2 class="sec-title">Library</h2>
+    <div class="item">
+      <div class="icon"></div>
+      <p>Liked Songs</p>
     </div>
-    <div class="playlist-items">
-      <p class="titles">liked songs</p>
+    <div class="item">
+      <div class="icon"></div>
+      <p>Followed Artists</p>
     </div>
+    <div class="item">
+      <div class="icon"></div>
+      <p>Liked Albums</p>
+    </div>
+
+    <h2 class="sec-title">Playlists</h2>
+    <div class="item">
+      <div class="icon"></div>
+      <p>Liked Playlists</p>
+    </div>
+    <div class="item">
+      <div class="icon"></div>
+      <p>Create Playlist</p>
+    </div>
+
   </div>
 </template>
 <style scoped>
-.playlist-items{
-  padding: 0 8px 0 8px;
+.library {
+  padding: 16px 16px 100px 16px;
+}
+.library h2 {
+  font-size: 18px;
+  font-weight: 600;
+}
+.item {
   display: flex;
   align-items: center;
-  height: 38px;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+  margin: 8px 0;
 }
-.titles{
-  text-transform: capitalize;
+.sec-title {
+  margin-top: 44px;
 }
+.item .icon {
+  width: 58px;
+  height: 58px;
+  background-color: #f2f2f2;
+  border-radius: 4px;
+}
+.item p {
+  margin-left: 8px;
+  font-size: 12px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+}
+
 </style>
+
+<script>
+import Song from '@/components/Song.vue'
+export default {
+  components: {
+    Song
+  },
+  computed: {
+    songs: function(){
+      return this.$store.state.songs
+    },
+  }
+}
+</script>
