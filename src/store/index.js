0670c395d75dc5dcc063cf982436cf3299fd20ac
= <pesaregoal@gmail.com>
2021-07-25 13:47:46 +0430

    test: github actions

--- src/store/index.js
+++ src/store/index.js
@@ -20,7 +20,7 @@ const store = new Vuex.Store({
     ],
     song: {},
     profile: {
-      user_firstname: 'No',
+      user_firstname: 'Guest',
       user_lastname: 'User'
     },
     artists: [
