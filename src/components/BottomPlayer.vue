764df3f46c0b1b8772e95cf3d9a053ba672e3dbe
= <pesaregoal@gmail.com>
2021-08-02 13:05:03 +0430

    fix: separate player control stylings

--- src/components/BottomPlayer.vue
+++ src/components/BottomPlayer.vue
@@ -201,33 +201,6 @@ audio{
     font-size: 32px;
     color: #ffc857
 }
-#player--control--wrapper {
-    display: none;
-    width: 86%;
-    height: 100%;
-    justify-content: space-between;
-    align-items: center;
-    color: #c1c1c1
-}
-#player--control--wrapper div {
-    min-width: 46px;
-    min-height: 46px;
-    display: flex;
-    place-content: center;
-    place-items: center;
-}
-.base-1 {
-    font-size: 24px;
-}
-.base-2 {
-    font-size: 30px;
-}
-.base-3 {
-    font-size: 36px;
-}
-.active {
-    color: #ffc857
-}
 #playing--top-nav {
     display: none;
     opacity: 0;
--- src/components/BottomPlayer/Control.vue
+++ src/components/BottomPlayer/Control.vue
@@ -46,6 +46,30 @@ export default {
 </script>
 
 <style scoped>
+#player--control--wrapper {
+    display: none;
+    width: 86%;
+    height: 100%;
+    justify-content: space-between;
+    align-items: center;
+    color: #c1c1c1
+}
+#player--control--wrapper div {
+    min-width: 46px;
+    min-height: 46px;
+    display: flex;
+    place-content: center;
+    place-items: center;
+}
+.base-1 {
+    font-size: 24px;
+}
+.base-2 {
+    font-size: 30px;
+}
+.base-3 {
+    font-size: 36px;
+}
 .active {
     color: #ffc857
 }
