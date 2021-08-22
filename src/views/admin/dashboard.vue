ab5ff98e4f59388c5546f608d3b2249561d58b63
= <pesaregoal@gmail.com>
2021-08-23 02:10:07 +0430

    fix: add admin action lists

---
+++ src/views/admin/dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dash">
+        <router-link to="/admin/access"> 
+            manage access
+        </router-link>
+
+        <router-link to="/admin/artists">
+            manage artists
+        </router-link>
+
+        <router-link to="/admin/songs">
+            manage songs
+        </router-link>
+    </div>
+</template>
+<style scoped>
+.dash a {
+    display: block;
+    color: #eee;
+    padding: 10px;
+    text-decoration: none;
+}
+</style>
