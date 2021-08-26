90b56177d3f376822f1bc9cb66e4259f571b3b36
= <pesaregoal@gmail.com>
2021-08-26 20:15:18 +0430

    fix: add some styling

--- src/views/admin/songs/upload.vue
+++ src/views/admin/songs/upload.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div id="trackCover"></div>
+        <div id="trackCover" @click="selectCover"></div>
         <input type="file" id="cover">
         <input type="text" id="trackTitle" v-model="trackTitle" placeholder="Track Title">
         <input type="text" id="trackArtist" v-model="trackArtist" placeholder="Track Artist">
@@ -21,6 +21,10 @@ export default {
     methods: {
         upload() {
             // audiofy.myren.xyz/api/v1/uploadSong?avatar=&
+        },
+        selectCover() {
+            let input = this.$el.querySelector('#cover')
+            input.click()
         }
     },
     mounted() {
@@ -37,8 +41,23 @@ export default {
 <style scoped>
 #trackTitle, #trackArtist {
     display: block;
+    background-color: #282828;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    color: #ABABAB;
+    min-height: 38px;
+    padding: 10px 16px;
+    min-width: 370px;
+}
+#trackArtist {
+    margin-top: 10px;
+    margin-bottom: 40px;
 }
 #trackCover {
+    background-color: #c1c1c1;
+    border-radius: 5px;
+    margin-bottom: 40px;
     outline: none;
     border: none;
     width: 160px;
@@ -47,5 +66,8 @@ export default {
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
+}
+#cover {
+    display: none;
 }
 </style>
