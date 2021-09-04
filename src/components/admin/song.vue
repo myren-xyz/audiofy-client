ece81f16338c5bbd115efe2f4f6842ae7ae4c9a2
= <pesaregoal@gmail.com>
2021-09-04 19:49:00 +0430

    feature: add song list for admin page

---
+++ src/components/admin/song.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="song-single">
+        <div class="song-single-container">
+
+            <div>
+                <p class="ss-name">{{song.title}}</p>
+            </div>
+
+            <div>
+                <p class="ss-username" v-for="artist in song.artists" :key="artist">{{artist}}</p>
+            </div>
+
+            <div class="song-action">
+                <router-link :to="`/admin/songs/edit/${song._id}`">
+                    <p class="edit-song">edit</p>
+                </router-link>
+               
+                <p class="delete-song" @click="deleteArtist">delete</p>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['song'],
+
+    methods: {
+        deleteArtist() {
+            console.log('delete artist');
+        }
+    },
+}
+</script>
+
+<style scoped>
+.song-single {
+    width: 100%;
+}
+.song-single-container {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    background-color: #282828;
+    padding: 20px;
+    border-radius: 5px;
+    margin-top: 10px;
+}
+.song-single-container div {
+    width: calc(100% / 3);
+}
+.song-action {
+    display: flex;
+    justify-content: flex-end;
+    cursor: pointer;
+}
+.delete-song {
+    color: #FF6157;
+    font-size: 11px;
+    margin-left: 18px
+}
+.edit-song {
+    color: #C1C1C1;
+    font-size: 11px;
+}
+.ss-username {
+    color: #C1C1C1;
+    font-weight: 100;
+}
+</style>
--- src/views/admin/songs.vue
+++ src/views/admin/songs.vue
@@ -1,5 +1,78 @@
 <template>
-    <div>
-        <p> hey</p>
+    <div class="song-list">
+        <div class="ss-topper">
+            <input type="text" id="song-searchbox" v-model="query" placeholder="search for song name"/>
+            <p class="ss-create-artist" @click="createSong">New Song</p>
+        </div>
+        <singleSong v-for="song in songs" :key="song._id" :song="song" />
     </div>
 </template>
+
+<script>
+import singleSong from '@/components/admin/song.vue'
+import axios from 'axios';
+export default {
+    components: {
+        singleSong
+    },
+
+    data() {
+        return {
+            query: '',
+            songs: [],
+        }
+    },
+    
+    methods: {
+        createSong() {
+            this.$router.push('/admin/songs/upload')
+        }
+    },
+
+    mounted() {
+        var timer
+        let searchbox = document.getElementById('song-searchbox');
+        searchbox.addEventListener('keyup', () => {
+            if (searchbox.value.length > 2) {
+                if(timer){
+                    clearTimeout(timer);
+                }
+                timer = setTimeout(() => {
+                    let searchURL = `https://audiofy.myren.xyz/api/v1/search?query=${this.query}&songs=true`;
+                    axios.get(searchURL).then(res => {
+                        if (res.data.ok) pushSongs(this, res.data.data);
+                    })
+                }, 369);
+            }
+        })
+    },
+}
+
+function pushSongs(vm, data) {
+    vm.songs = data.Songs;
+}
+</script>
+
+<style scoped>
+.ss-topper {
+    margin: 20px 0 20px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.ss-create-artist {
+    color: #ffc857;
+    cursor: pointer;
+}
+#song-searchbox {
+    display: block;
+    width: 240px;
+    height: 38px;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    background-color: #282828;
+    color: #fff;
+}
+</style>
